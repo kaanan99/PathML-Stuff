581871160837ba68736068a2f625d50cf389f7c4
Kaanan Kharwa <kaanan99@gmail.com>
2024-03-24 20:51:06 +0000

    Fixed bugs in start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -2,8 +2,8 @@ docker run \
         --runtime=nvidia\
         --gpus all \
         -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video \
-        -v /data:/mnt/durable
-        -v /mnt/ephemeral/:/mnt/ephemeral
+        -v /data:/mnt/durable \
+        -v /mnt/ephemeral/:/mnt/ephemeral \
         -p 8892:8888 \
         -p 8893:6006 \
         --name \
