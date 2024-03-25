2fc51777a7bd3a88f3ddf999707073c1a3e2ed6a
Kaanan Kharwa <kaanan99@gmail.com>
2024-03-25 22:16:47 +0000

    Updated config for efficientnet training

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@ TASK = 1
 NUM_CLASSES = 3
 FPS = 1
 
-TRAIN_FILE_LIST = f'/data/task1_training.txt'
-TEST_FILE_LIST = f'/data/task1_testing.txt'
-EVAL_FILE_LIST = f'/data/task1_eval.txt'
+TRAIN_FILE_LIST = f'/mnt/durable/training_data/filelists/task1_training.txt'
+TEST_FILE_LIST = f'/mnt/durable/training_data/filelists/task1_testing.txt'
+EVAL_FILE_LIST = f'/mnt/durable/training_data/filelists/task1_eval.txt'
 
 TRAIN_SIZE =  350160 * FPS
 TEST_SIZE  =   53642 * FPS
@@ -16,7 +16,7 @@ NUM_TO_LABEL = {
     2: "standing"
 }
 
-SEQUENCE_LENGTH = 10
+SEQUENCE_LENGTH = 1 # Change back for LCRN
 BATCH_SIZE = 40
 VAL_BATCH_SIZE = 10
 EPOCHS = 15
@@ -25,7 +25,7 @@ EPOCHS = 15
 HIDDEN_SIZE = 64
 NUM_HIDDEN_LAYERS = 2
 
-EXPERIMENT_NAME = "LRCN_custom_efficientnet_v1"
+EXPERIMENT_NAME = "custom_efficientnet_classweights"
 
 CLASS_WEIGHTS = True
 FAST_DEV_RUN = False
